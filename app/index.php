b36d9a87b66bd3f2bb123fa970b0b9256882d1a2
Axel Verstappen <axel.verstappen@gmail.com>
2016-02-28 12:40:30 +0100

    Init sass files

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
     <title>Home | Flash Guns - Hitmen</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/css/screen.css">
+    <link rel="stylesheet" href="/stylesheets/css/screen.css">
 </head>
 <body>
     <?php include_once('/includes/nav.include.php'); ?>
