a2ef0a473a6bea67e1642b3e5a73999ecf72e232
Axel Verstappen <axel.verstappen@gmail.com>
2016-05-08 14:44:40 +0200

    Removed team links from header

--- app/includes/nav.include.php
+++ app/includes/nav.include.php
@@ -6,18 +6,6 @@
                     <li><a href="http://www.rbihf.be/"><img src="/images/links/rbihf.png" alt="rbihf"/></a></li>
                     <li><a href="http://www.nhl.com/"><img src="/images/links/nhl.png" alt="nhl"/></a></li>
                     <li><a href="http://www.iihf.com/"><img src="/images/links/iihf.png"alt="iihf"/></a></li>
-                    <li><a href="http://www.phantoms.be/"><img src="/images/links/phantoms.png" alt="phantoms"/></a></li>
-                    <li><a href="http://www.ihcl.be/"><img src="/images/links/chiefs.png" alt="chiefs"/></a></li>
-                    <li><a href="http://www.hyc.be/"><img src="/images/links/hyc.png" alt="hyc"/></a></li>
-                    <li><a href="http://www.whitecaps.be/"><img src="/images/links/whitecaps.png" alt="whitecaps"/></a></li>
-                    <li><a href="http://www.olympiaheist.be/v3/"><img src="/images/links/olympia.png" alt="olympia"/></a></li>
-                    <li><a href="http://www.haskeyhasselt.be/"><img src="/images/links/haskey.png" alt="haskey"/></a></li>
-                    <li><a href="http://www.liedekerkelions.be/"><img src="/images/links/lions.png" alt="lions"/></a></li>
-                    <li><a href="http://www.thejets.be/"><img src="/images/links/jets.png" alt="jets"/></a></li>
-                    <li><a href="http://www.redroosterscharleroi.be/"><img src="/images/links/charleroi.png" alt="charleroi"/></a></li>
-                    <li><a href="http://www.cold-play.be/"><img src="/images/links/coldplay.png" alt="coldplay"/></a></li>
-                    <li><a href="http://www.moosemaaseik.com/"><img src="/images/links/maaseik.png" alt="maaseik"/></a></li>
-                    <li><a href="http://www.bulldogs-hockey.be/"><img src="/images/links/bulldogs.png" alt="bulldogs"/></a></li>
                 </ul>
             </div>
         </div>
